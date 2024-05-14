205c3937284a6f4a5d89af0ab4a3a58b0050ea0f
thedorktrain <mackemosabe@gmail.com>
2024-05-13 20:56:47 -0500

    added main.handlebars and homepage.handlebars templates

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: "Supersecretsecret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
