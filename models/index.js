2e5148708c3bb46a4f822b21464af9248ed9acb4
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-05-15 10:05:11 -0500

    i made changes to the structure of the project most of it should still be the same though

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // TODO: Import any models and define additional relationships here
-const Gossip = require("./gossip");
+const Gossip = require('./gossip');
 const User = require('./User');
-const userData = require('./../seeds/userData.json')
 
-module.exports = { User, Gossip, userData };
+
+module.exports = { User, Gossip };
--- public/js/login.js
+++ public/js/login.js
@@ -12,49 +12,22 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      const data = await response.json();
-      alert(data.message);
-      // Redirect to the specified URL after successful login
-      window.location.href = data.redirectTo || '/homepage';
+      document.location.replace('/homepage');
     } else {
       alert('Failed to log in');
     }
   }
 };
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document
+.querySelector('.login-form')
+.addEventListener("click", loginFormHandler);
 
-const signupFormHandler = async (event) => {
-  // Stop the browser from submitting the form so we can do so with JavaScript
-  event.preventDefault();
-
-  // Gather the data from the form elements on the page
-  const name = document.querySelector('#userSignup').value.trim();
-  const email = document.querySelector('#emailSignup').value.trim();
-  const password = document.querySelector('#passwordSignup').value.trim();
-
-  if (name && email && password) {
-    // Send the e-mail and password to the server
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to log in');
-    }
-  }
-};
-
-document.getElementById("logButt").addEventListener("click", function() {
+document
+.getElementById("logButt")
+.addEventListener("click", function() {
   window.location.href = "/login";
 });
 
-// document
-//   .querySelector('.login-form')
-//   .addEventListener('submit', loginFormHandler);
 
-document.getElementById("signUpForm").addEventListener('click', signupFormHandler);
+
